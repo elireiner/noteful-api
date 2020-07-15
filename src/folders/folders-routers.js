6d7fa80774e849f8c7e1bb998f7616542d1bf8e0
Eli Reiners <goelyukim@gmail.com>
2020-07-15 07:42:04 -0400

    Complete folder routes

--- src/folders/folders-routers.js
+++ src/folders/folders-routers.js
@@ -20,14 +20,86 @@ foldersRouter
                 res.json(folders.map(serialize))
             })
             .catch(next)
-        })
-    .post()
+    })
+    .post(jsonParser, (req, res, next) => {
+        const { folder_name } = req.body;
+        const newFolder = { folder_name };
+
+        if (!folder_name) {
+            return res.status(400).json({
+                error: { message: `Missing 'folder_name' in request body` }
+            })
+        }
+
+        FoldersService.insertFolder(
+            req.app.get('db'),
+            newFolder
+        )
+
+            .then(folder => {
+                res
+                    .status(201)
+                    .location(path.posix.join(req.originalUrl, `/${folder.id}`))
+                    .json(serialize(folder))
+            })
+            .catch(next)
+    })
 
 foldersRouter
     .route('/folder/:folder_id')
-    .all()
-    .get()
-    .delete()
-    .patch()
+    .all((req, res, next) => {
+        FoldersService.getById(
+            req.app.get('db'),
+            req.params.folder_id
+        )
+            .then(folder => {
+                if (!folder) {
+                    return res.status(404).json({
+                        error: { message: `Folder does not exist` }
+                    })
+                }
+                res.folder = folder;
+                next()
+            })
+            .catch(next)
+
+    })
+    .get((req, res, next) => {
+        res.json({
+            id: res.folder.id,
+            folder_name: xss(res.folder_name), // sanitize title
+        })
+    })
+    .delete(jsonParser, (req, res, next) => {
+        FoldersService.deleteFolder(req.app.get('db'),
+            req.params.folder_id)
+            .then(numRowsAffected => {
+                res.status(204).end()
+            })
+            .catch(next)
+    })
+    .patch(jsonParser, (req, res, next) => {
+        const { folder_name } = req.body;
+        const folderToUpdate = { folder_name }
+
+     
+        if (!folder_name) {
+            return res.status(400).json({
+                error: {message: `Request body must contain a folder_name `}
+            })
+        }
+        
+        FoldersService.updateFolder(
+            req.app.get('db'),
+            req.params.folder_id,
+            folderToUpdate
+        )
+
+            .then(numRowsAffected => {
+                res.status(204).end()
+            })
+
+            .catch(next)
+    })
 
 module.exports = foldersRouter
